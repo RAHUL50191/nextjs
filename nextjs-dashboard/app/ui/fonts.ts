20b355d35e277460f8ad652d2ae2f60eed1405ed
Rahul <rahul200283@gmail.com>
2024-02-25 12:08:14 +0530

    main

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter ,Lusitana,Bebas_Neue} from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana=Lusitana({subsets:['latin'],weight:"400"});
+export const bebas_Neue=Bebas_Neue({subsets:['latin'],weight:"400"});
+ 
